c53a6f6ba1d9529e3c1c677e7390fcb09fdbfa74
Langat18 <langatclement18@gmail.com>
2025-12-01 18:22:53 +0300

    Fix: Add imports to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,10 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from "./pages/RegisterPage";
 import StudentRegisterPage from "./pages/StudentRegisterPage";
 import StaffRegisterPage from "./pages/StaffRegisterPage";
+import StudentDashboard from "./pages/student/StudentDashboard";
 import CounselorDashboard from "./pages/CounselorDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
@@ -18,7 +20,13 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/register/student" element={<StudentRegisterPage />} />
           <Route path="/register/staff" element={<StaffRegisterPage />} />
+          
+          <Route path="/student/dashboard" element={<StudentDashboard />} />
+          <Route path="/student/chat/:ticketId" element={<ChatPage />} />
+          
           <Route path="/counselor/dashboard" element={<CounselorDashboard />} />
+          <Route path="/counselor/chat/:ticketId" element={<ChatPage />} />
+          
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
         </Routes>
       </AuthProvider>
